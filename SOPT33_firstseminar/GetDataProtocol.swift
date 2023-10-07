c428211135cb18a5ee9c23aebeef1b5ab3930b09
mcrkgus <mcrkgus@naver.com>
2023-10-07 17:20:43 +0900

    first seminar

---
+++ SOPT33_firstseminar/GetDataProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  GetDataProtocol.swift
+//  SOPT33_firstseminar
+//
+//  Created by Gahyun Kim on 2023/10/07.
+//
+
+import Foundation
+
+
+protocol GetDataProtocol {
+    func getLoginData(email: String, password: String)
+}
+
+
--- SOPT33_firstseminar/ViewController.swift
+++ SOPT33_firstseminar/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+
     
     private var idText: String = ""
     private var passwordText: String = ""
@@ -36,9 +37,32 @@ class ViewController: UIViewController {
     
     
     @IBAction func loginButton(_ sender: Any) {
-        print("HELLO")
+        print("\(idText)\n\(passwordText)")
+        pushToResultVC()
     }
-  
+    //segue로 관리 -> 쉬움 / 복잡, 유지보수 어려워짐 
     
+    func pushToResultVC() {
+    guard let resultVC = self.storyboard?.instantiateViewController(withIdentifier: "ResultViewController") as? ResultViewController else {return}
+        
+        resultVC.email = idText
+        resultVC.password = passwordText
+        
+        resultVC.delegate = self
+        resultVC.loginDataCompletion = { data in
+            print("클로저로 받아온 email : \(data[0]), 클로저로 받아온 password : \(data[1])")
+        }
+        
+        self.navigationController?.pushViewController(resultVC, animated: true)
+        //self.present(resultVC, animated: true)  //뒤로가기 버튼 눌러도 화면 전환이 안됨
+        
+        
+        
+    }
 }
 
+extension ViewController: GetDataProtocol {
+    func getLoginData(email: String, password: String) {
+        print("받아온 email : \(email), 받아온 password : \(password)")
+    }
+}
