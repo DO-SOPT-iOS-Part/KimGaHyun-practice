b8bde56721672fb4cd0b9b8d6588a9c159520a2b
mcrkgus <mcrkgus@naver.com>
2023-10-07 15:23:25 +0900

    test commit

--- SOPT33_firstseminar/ViewController.swift
+++ SOPT33_firstseminar/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var idText: String = ""
+    private var passwordText: String = ""
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func emailTextField(_ sender: Any) {
+        guard let textField = sender as? UITextField else {return}
+        if let idText = textField.text {
+            self.idText = idText
+        }
+    }
+    
+    
+    @IBAction func passwordTextField(_ sender: Any) {
+        guard let textField = sender as? UITextField else {return}
+        if let passwordText = textField.text {
+            self.passwordText = passwordText
+        }
+    }
+    
+    
+    @IBAction func loginButton(_ sender: Any) {
+        print("HELLO")
+    }
+  
+    
 }
 
