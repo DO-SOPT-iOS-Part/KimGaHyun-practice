a569055917771d4ecb425aa80f6e32f8ba871e3b
mcrkgus <mcrkgus@naver.com>
2023-10-08 17:05:40 +0900

    [WEEK1] UIComponent 추가

--- SOPT33_WEEK1/SOPT33_WEEK1/ViewController.swift
+++ SOPT33_WEEK1/SOPT33_WEEK1/ViewController.swift
@@ -25,6 +25,7 @@ class ViewController: UIViewController {
         if let idText = textField.text {
             self.idText = idText
         }
+        textField.clearButtonMode = .always     //textField의 오른쪽 -> 입력 내용 한 번에 삭제
     }
     
     @IBAction func passwordTextField(_ sender: Any) {
@@ -32,11 +33,20 @@ class ViewController: UIViewController {
         if let passwordText = textField.text {
             self.passwordText = passwordText
         }
+        textField.clearButtonMode = .always     //textField의 오른쪽 -> 입력 내용 한 번에 삭제
+        textField.isSecureTextEntry = true  //비밀번호 숨겨서 보여주기
     }
     
-    @IBAction func loginButton(_ sender: Any) {
+    @IBAction func loginButton(_ sender: UIButton) {
         print("\(idText)\n\(passwordText)")
+        sender.backgroundColor = UIColor.black  //button 배경 색
+        sender.tintColor = UIColor.white    //button 글자 색
+        sender.layer.masksToBounds = true   //button 둥글게
+        sender.layer.cornerRadius = 8       //button 테두리 반지름 -> 클수록 둥글어짐
+        
+        
         pushToResultVC()
+        
     }
     //segue로 관리 -> 쉬움 / 복잡, 유지보수 어려워짐
     
@@ -50,7 +60,7 @@ class ViewController: UIViewController {
         resultVC.loginDataCompletion = { data in
             print("클로저로 받아온 email : \(data[0]), 클로저로 받아온 password : \(data[1])")
         }
-        
+
         self.navigationController?.pushViewController(resultVC, animated: true)
         //self.present(resultVC, animated: true)  //뒤로가기 버튼 눌러도 화면 전환이 안됨
     }
