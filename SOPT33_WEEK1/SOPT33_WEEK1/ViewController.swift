95a68eae342934677b4203a1383205f6e96e3f50
mcrkgus <mcrkgus@naver.com>
2023-10-08 14:50:03 +0900

    test

---
+++ SOPT33_WEEK1/SOPT33_WEEK1/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SOPT33_WEEK1
+//
+//  Created by Gahyun Kim on 2023/10/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
