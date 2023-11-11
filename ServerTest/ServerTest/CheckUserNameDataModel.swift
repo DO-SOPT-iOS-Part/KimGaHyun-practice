e31c9d197f3725be3ed041ba4598ca58f5a126e1
mcrkgus <mcrkgus@naver.com>
2023-11-11 17:10:33 +0900

    [Feat] CheckUserName을 위한 Data, Service

---
+++ ServerTest/ServerTest/CheckUserNameDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  CheckUserNameDataModel.swift
+//  ServerTest
+//
+//  Created by Gahyun Kim on 2023/11/11.
+//
+
+import Foundation
+
+// MARK: - Welcome
+struct CheckUserNameDataModel: Codable {
+    let isExist: Bool
+}
